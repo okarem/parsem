0743450a5eba0c6e0524ace5e91cc2e62c53f27b
okarem <karem.omary@gmail.com>
2019-09-29 18:25:54 +0300

    structure and some foldwers made+ index.html+ featutres.js

---
+++ assets/scripts/features.js
@@ -0,0 +1,9 @@
+(function writeHello(){ console.log("hello!");}
+)();
+
+function makeSearcBar(parenStr){
+    if(typeof(parenStr)!='string'){return;} //throw an error or return something appropriate
+    var parent= document.getElementById(parenStr);
+    
+
+}
