3c28009df3fa364e5dcfb2179f8abd43ad289d30
okarem <okarem@users.noreply.github.com>
2019-10-27 22:04:33 +0200

    added solution

--- assets/scripts/features.js
+++ assets/scripts/features.js
@@ -1,9 +1,28 @@
-(function writeHello(){ console.log("hello!");}
-)();
+(function writeHello(){ console.log('hello!');
+	makeSearchBar(document.querySelector('body'));
+})();
 
-function makeSearcBar(parenStr){
-    if(typeof(parenStr)!='string'){return;} //throw an error or return something appropriate
-    var parent= document.getElementById(parenStr);
-    
-
+function makeSearchBar(parent){
+    // if(typeof(parent)!='string'){return;} //throw an error or return something appropriate
+    // var parentNode= document.getElementById(parent);
+    var form=document.createElement('span');
+    var searchInput = document.createElement('input');
+    searchInput.setAttribute('type', 'text');
+    searchInput.setAttribute('id', 'search-input-box');
+    searchInput.setAttribute('placeholder', 'what\'re you looking for?');
+    searchInput.classList.add('search-input-box');
+    parent.appendChild(searchInput);
+    //making the Button
+    var submitButton = document.createElement('button');
+    submitButton.addEventListener('click',submitButtonFunction);
+    submitButton.textContent= "Search";
+    parent.appendChild(submitButton);
+}
+function submitButtonFunction(){
+	var textBox = document.getElementById('search-input-box');
+	var str= textBox.value;	//finished getting data
+	textBox.value = textBox.defaultValue; //finished reseting the textBox
+	
+	//process the data from textBox
+	console.log(str);
 }
